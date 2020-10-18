f6e12df17f3eabff1dafcdd4b469584c86afd755
Rexfortiz <daydreamer9422@gmail.com>
2020-10-19 01:27:15 +0700

    Tugas Minggu 6
    Thomas Farel 01918147226

---
+++ Assets/Script/Keluar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keluar : MonoBehaviour
+{   
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Script/MunculkanSampah.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MunculkanSampah : MonoBehaviour
+{
+    public float jeda = 0.8f;
+    float timer;
+    public GameObject[] objekSampah;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > jeda)
+        {
+            int random = Random.Range(0, objekSampah.Length);
+            Instantiate(objekSampah[random], transform.position, transform.rotation);
+            timer = 0;
+        }
+    }
+}
